cc39c2d8dd3b4a45ba9b8b5c57ca411c212ebbd3
GiuseppeGambacorta <gambacortagiuseppe@outlook.it>
2025-11-30 12:25:20 +0100

    Initial Commit

---
+++ PodmanIconBar/PodmanIconBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodmanIconBarApp.swift
+//  PodmanIconBar
+//
+//  Created by Giuseppe Gambacorta on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PodmanIconBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodmanIconBarTests/PodmanIconBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  PodmanIconBarTests.swift
+//  PodmanIconBarTests
+//
+//  Created by Giuseppe Gambacorta on 30/11/25.
+//
+
+import Testing
+@testable import PodmanIconBar
+
+struct PodmanIconBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
